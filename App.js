114ba2d4042b3e1423fd3f4ea833521fedd06060
Roman Ustyantsev <schoolarts9ru@gmail.com>
2019-03-17 23:00:52 +0800

    add redux

---
+++ App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider, connect } from 'react-redux';
+import axios from 'axios';
+import axiosMiddleware from 'redux-axios-middleware';
+
+import reducer from './components/reducer';
+import RepoList from './components/RepoList';
+
+const client = axios.create({
+  baseURL: 'https://api.github.com',
+  resonseType: 'json'
+});
+
+const store = createStore(reducer, applyMiddleware(axiosMiddleware(client)));
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <View style={styles.container}>
+          <RepoList />
+        </View>
+      </Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    marginTop: 50
+  }
+});
