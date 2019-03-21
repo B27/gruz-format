b99f487288cc3bb7e113f231a1686b51a4d8c51a
Roman Ustyantsev <schoolarts9ru@gmail.com>
2019-03-21 18:46:51 +0800

    design loginPage and problem with keyboard

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -2,48 +2,95 @@ import React from 'react';
 import {
     StyleSheet,
     View,
-    Button,
     AsyncStorage,
     TextInput,
     Text,
     ImageBackground,
-    Image
+    Dimensions,
+    TouchableOpacity,
+    KeyboardAvoidingView
 } from 'react-native';
 import bgImage from '../images/background.png';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
-import Icon from 'react-native-vector-icons/MaterialIcons'
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import Icon2 from 'react-native-vector-icons/Ionicons';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+
+const { width: WIDTH } = Dimensions.get('window');
 
 class SignInScreen extends React.Component {
     state = {
-        text: ''
+        login: '',
+        password: '',
+        showPass: true,
+        press: false
     }
     static navigationOptions = {
         header: null
     };
 
+    showPass = () => {
+        if (this.state.press == false) {
+            this.setState({ showPass: false, press: true })
+        } else {
+            this.setState({ showPass: true, press: false })
+        }
+    }
+
     render() {
         return (
-            <ImageBackground source={bgImage} style={styles.backgroundContainer}>
-                <View style={styles.logoContainer}>
-                    <Text style={styles.logoText}>
-                        ДОБРО {"\n"}ПОЖАЛОВАТЬ
+            <KeyboardAwareScrollView
+                style={{flex: 1}}
+                contentContainerStyle={{ flex: 1 }}
+                resetScrollToCoords={{ x: 0, y: 0 }}
+                enableResetScrollToCoords='true'
+                enableOnAndroid='true'
+                enableAutomaticScroll='true'
+            >
+                <ImageBackground source={bgImage} style={styles.backgroundContainer}>
+                    <View style={styles.logoContainer}>
+                        <Text style={styles.logoText}>
+                            ДОБРО {"\n"}ПОЖАЛОВАТЬ
                     </Text>
-                    <Icon name={'fiber-manual-record'} size={28} color={'black'} style={styles.logoIcon}/>
-                    <Icon name={'fiber-manual-record'} size={28} color={'white'} style={styles.logoIcon}/>
-                </View>
-                <View style={styles.inputContainer}>
-                    
-                    <TextInput
-                        style={{ height: 40 }}
-                        placeholder="Type here to translate!"
-                        onChangeText={(text) => this.setState({ text })}
-                    />
-                    <Text style={{ padding: 10, fontSize: 42 }}>
-                        {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-                    </Text>
-                    <Button title='Войти' onPress={() => this._signInAsync()} />
-                </View>
-            </ImageBackground>
+                        <Icon name={'fiber-manual-record'} size={42} color={'black'} style={styles.logoIcon} />
+                        <Icon name={'fiber-manual-record'} size={42} color={'white'} style={styles.logoIcon} />
+                    </View>
+                    <View style={styles.inputBlock}>
+
+                        <View style={styles.inputContainer} behavior="padding" enabled>
+
+                            <Icon name={'person'} size={28} color="#FFC234" style={styles.inputIcon} />
+                            <TextInput
+                                style={styles.input}
+                                placeholder="Логин"
+                                placeholderTextColor="grey"
+                                onChangeText={(login) => this.setState({ login })}
+                            />
+
+                        </View>
+
+                        <View style={styles.inputContainer}>
+                            <Icon name={'lock'} size={28} color="#FFC234" style={styles.inputIcon} />
+                            <TextInput
+                                style={styles.input}
+                                placeholder="Пароль"
+                                secureTextEntry={this.state.showPass}
+                                placeholderTextColor="grey"
+                                onChangeText={(password) => this.setState({ password })}
+                            />
+                            <TouchableOpacity style={styles.btnEye}
+                                onPress={this.showPass.bind(this)}>
+                                <Icon2 name={this.state.press == false ? 'md-eye' : 'md-eye-off'} size={26} color="grey" />
+                            </TouchableOpacity>
+                        </View>
+
+                        <TouchableOpacity style={styles.btnLogin} onPress={() => this._signInAsync()}>
+                            <Text style={styles.text} >ВОЙТИ</Text>
+                        </TouchableOpacity>
+
+                    </View>
+                </ImageBackground>
+            </KeyboardAwareScrollView>
         );
     }
 
@@ -55,30 +102,63 @@ class SignInScreen extends React.Component {
 
 const styles = StyleSheet.create({
     backgroundContainer: {
-        flex: 1,
+        flex:1,
         width: null,
         height: null,
     },
     logoContainer: {
-        
-       
         marginLeft: 24,
-        borderWidth: 2,
-        borderColor: 'orange',
         flex: 1,
-        justifyContent: 'center'
+        justifyContent: 'flex-end',
+        paddingBottom: '5%'
     },
     logoText: {
         color: 'white',
-        fontSize: 30,
+        fontSize: 32,
         fontWeight: '500',
-        
+    },
+    logoIcon: {
+        marginTop: 15
+    },
+    inputBlock: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: "center"
+    },
+    input: {
+        width: WIDTH - 55,
+        height: 45,
+        borderWidth: 1,
+        borderRadius: 15,
+        fontSize: 16,
+        paddingLeft: 45,
+        marginHorizontal: 25
+    },
+    inputIcon: {
+        position: 'absolute',
+        top: 8,
+        left: 37
     },
     inputContainer: {
-        flex: 1,
-        borderWidth: 2,
-        borderColor: 'orange',
-        justifyContent: 'center'
+        marginTop: 10
+    },
+    btnEye: {
+        position: 'absolute',
+        top: 8,
+        right: 37
+    },
+    btnLogin: {
+        width: WIDTH / 2,
+        height: 45,
+        borderRadius: 25,
+        backgroundColor: 'black',
+        justifyContent: 'center',
+        marginTop: 20
+    },
+    text: {
+        color: '#FFC234',
+        fontSize: 16,
+        textAlign: 'center'
     }
 })
 export default SignInScreen;
