4201616ea19144c844e1a7f445a5b4589f601738
B27 <ilia.burdukovski@ya.ru>
2019-06-04 13:26:31 +0800

    some pretty onChangeStarRating

--- components/StarRating.js
+++ components/StarRating.js
@@ -1,15 +1,19 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Easing } from 'react-native';
 import Rating from 'react-native-rating';
 
 export default class StarRating extends React.Component {
+    onChangeRating = rating => {
+        this.props.onChange(this.props.id, rating);
+    };
+
     render() {
         const starFilled = require('../images/round_star_yellow_36dp.png');
         const starUnfilled = require('../images/round_star_border_yellow_36dp.png');
 
         return (
             <Rating
-                onChange={this.props.onChange}
+                onChange={onChangeRating}
                 selectedStar={starFilled}
                 unselectedStar={starUnfilled}
                 config={{
