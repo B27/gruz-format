1910dc3a187781e400520042e8b3141f11636800
B27 <ilia.burdukovski@ya.ru>
2019-06-21 17:58:45 +0800

    add ask location permission

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,201 +1,205 @@
-import Axios from 'axios';
+import AsyncStorage from '@react-native-community/async-storage';
+import * as Permissions from 'expo-permissions';
 // import { Constants, Location, Notifications, Permissions, TaskManager } from 'expo';
 import { inject, observer } from 'mobx-react/native';
 import React from 'react';
-import { FlatList, Platform, Text, View, YellowBox } from 'react-native';
-import AsyncStorage from '@react-native-community/async-storage';
+import { Alert, FlatList, NativeModules, Platform, Text, View, YellowBox } from 'react-native';
 import OrderCard from '../components/OrderCard';
-import { getSocket } from '../components/Socket';
 import SwitchToggle from '../components/SwitchToggle';
 import styles from '../styles';
-import { NativeModules } from 'react-native';
 
 // const LOCATION_TASK_NAME = 'background-location-task';
 
 YellowBox.ignoreWarnings([
-	'Unrecognized WebSocket connection option(s) `agent`, `perMessageDeflate`, `pfx`, `key`, `passphrase`, `cert`, `ca`, `ciphers`, `rejectUnauthorized`. Did you mean to put these under `headers`?'
+    'Unrecognized WebSocket connection option(s) `agent`, `perMessageDeflate`, `pfx`, `key`, `passphrase`, `cert`, `ca`, `ciphers`, `rejectUnauthorized`. Did you mean to put these under `headers`?'
 ]);
 
 @inject('store')
 @observer
 class MainScreen extends React.Component {
-	state = {
-		notification: {},
-		workingStatus: false,
-		refreshing: false,
-		message: ''
-	};
+    state = {
+        notification: {},
+        workingStatus: false,
+        refreshing: false,
+        message: ''
+    };
 
-	componentDidMount = async () => {
-		// this._notificationSubscription = Notifications.addListener(this._handleNotification);
-		// await this.props.store.updateUserInfo()
-		// await store.getOrders();
+    componentDidMount = async () => {
+        // this._notificationSubscription = Notifications.addListener(this._handleNotification);
+        // await this.props.store.updateUserInfo()
+        // await store.getOrders();
 
-		// const socket = await getSocket();
-		// if (!socket || !socket.connected) {
-		//     setTimeout(()=>this.setState({message: 'Нет соединения с сервером'}), 2000)
-		// } else this.setState({message: ''})
+        // const socket = await getSocket();
+        // if (!socket || !socket.connected) {
+        //     setTimeout(()=>this.setState({message: 'Нет соединения с сервером'}), 2000)
+        // } else this.setState({message: ''})
 
-		this.willFocusSubscription = this.props.navigation.addListener('willFocus', () => {
-			this.setState({ message: '' });
-		});
-	};
+        this.willFocusSubscription = this.props.navigation.addListener('willFocus', () => {
+            this.setState({ message: '' });
+        });
+    };
 
-	componentWillUnmount() {
-		if (this.willFocusSubscription) {
-			this.willFocusSubscription.remove();
-		}
-	}
+    componentWillUnmount() {
+        if (this.willFocusSubscription) {
+            this.willFocusSubscription.remove();
+        }
+    }
 
-	// _getLocationAsync = async () => {
-	// 	let { status } = await Permissions.askAsync(Permissions.LOCATION);
-	// 	if (status !== 'granted') {
-	// 		this.setState({
-	// 			errorMessage: 'Permission to access location was denied'
-	// 		});
-	// 	}
-	// 	//let location = await Location.getCurrentPositionAsync({});
+    // _getLocationAsync = async () => {
+    // 	let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    // 	if (status !== 'granted') {
+    // 		this.setState({
+    // 			errorMessage: 'Permission to access location was denied'
+    // 		});
+    // 	}
+    // 	//let location = await Location.getCurrentPositionAsync({});
 
-	// 	await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
-	// 		accuracy: Location.Accuracy.High
-	// 	});
-	// };
+    // 	await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+    // 		accuracy: Location.Accuracy.High
+    // 	});
+    // };
 
-	// _handleNotification = async notification => {
-	// 	if (notification.origin === 'selected' && notification.data.type === 'accept') {
-	// 		const res = await Axios.get(`/order/${notification.data.order_id}`);
+    // _handleNotification = async notification => {
+    // 	if (notification.origin === 'selected' && notification.data.type === 'accept') {
+    // 		const res = await Axios.get(`/order/${notification.data.order_id}`);
 
-	// 		//this.setState({ notification: notification, order: res.data });
-	// 		//const data = this.state.notification.data;
-	// 		// console.log('origin: ' + this.state.notification.origin);
-	// 		// console.log('data: ' + (data && data.order_id));
-	// 		this.props.navigation.navigate('OrderPreview', { order: res.data });
-	// 	} else if (notification.origin === 'received' && notification.data.type === 'reject') {
-	// 		console.log('Заказ отменен');
+    // 		//this.setState({ notification: notification, order: res.data });
+    // 		//const data = this.state.notification.data;
+    // 		// console.log('origin: ' + this.state.notification.origin);
+    // 		// console.log('data: ' + (data && data.order_id));
+    // 		this.props.navigation.navigate('OrderPreview', { order: res.data });
+    // 	} else if (notification.origin === 'received' && notification.data.type === 'reject') {
+    // 		console.log('Заказ отменен');
 
-	// 		this.props.navigation.navigate('AuthLoading');
-	// 	} else if (notification.origin === 'received' && notification.data.type === 'kicked') {
-	// 		console.log('Вас выпилили из заказа');
+    // 		this.props.navigation.navigate('AuthLoading');
+    // 	} else if (notification.origin === 'received' && notification.data.type === 'kicked') {
+    // 		console.log('Вас выпилили из заказа');
 
-	// 		this.props.navigation.navigate('AuthLoading');
-	// 	}
-	// 	//console.log(notification.origin, notification.data.type);
-	// };
+    // 		this.props.navigation.navigate('AuthLoading');
+    // 	}
+    // 	//console.log(notification.origin, notification.data.type);
+    // };
 
-	// static navigationOptions = ({ navigation }) => ({
-	//     headerLeft: <MenuIcon navigationProps={navigation} />,
-	//     headerLeftContainerStyle: { paddingLeft: 8 }
-	// });
+    // static navigationOptions = ({ navigation }) => ({
+    //     headerLeft: <MenuIcon navigationProps={navigation} />,
+    //     headerLeftContainerStyle: { paddingLeft: 8 }
+    // });
 
-	render() {
-		const { store } = this.props;
-		return (
-			<FlatList
-				ListHeaderComponent={
-					<View>
-						<View style={styles.mainTopBackground}>
-							<Text style={{ color: 'red', textAlign: 'center', fontSize: 16 }}>
-								{this.state.message}
-							</Text>
-							<Text style={styles.mainFontUserName}>{store.name}</Text>
-							<Text style={styles.mainFontUserType}>{store.isDriver ? 'Водитель' : 'Грузчик'}</Text>
-							{/* <Context.Consumer>
+    render() {
+        const { store } = this.props;
+        return (
+            <FlatList
+                ListHeaderComponent={
+                    <View>
+                        <View style={styles.mainTopBackground}>
+                            <Text style={{ color: 'red', textAlign: 'center', fontSize: 16 }}>
+                                {this.state.message}
+                            </Text>
+                            <Text style={styles.mainFontUserName}>{store.name}</Text>
+                            <Text style={styles.mainFontUserType}>{store.isDriver ? 'Водитель' : 'Грузчик'}</Text>
+                            {/* <Context.Consumer>
 								{value => <Text style={styles.mainFontBalance}>{`${value.balance} руб.`}</Text>}
 							</Context.Consumer> */}
 
-							<Text style={styles.mainFontBalance}>{`${store.balance} руб.`}</Text>
+                            <Text style={styles.mainFontBalance}>{`${store.balance} руб.`}</Text>
 
-							<Text style={styles.mainFontTopUpBalance} onPress={this._topUpBalance}>
-								Пополнить баланс
-							</Text>
-						</View>
-						<View style={styles.mainWorkingItem}>
-							<Text style={styles.drawerFontTopItem}>Работаю</Text>
-							<View>
-								<SwitchToggle switchOn={store.onWork} onPress={this._onChangeSwitchValue} />
-							</View>
-						</View>
-					</View>
-				}
-				keyExtractor={this._keyExtractor}
-				data={store.orders.slice().sort((a, b) => a.start_time < b.start_time)} // возможно, эта сортировка когда-то будет работать неправильно
-				renderItem={this._renderItem}
-				refreshing={this.state.refreshing}
-				onRefresh={this._onRefresh}
-				ListEmptyComponent={<Text style={styles.mainFontUserType}>Нет доступных заявок</Text>}
-			/>
-		);
-	}
+                            <Text style={styles.mainFontTopUpBalance} onPress={this._topUpBalance}>
+                                Пополнить баланс
+                            </Text>
+                        </View>
+                        <View style={styles.mainWorkingItem}>
+                            <Text style={styles.drawerFontTopItem}>Работаю</Text>
+                            <View>
+                                <SwitchToggle switchOn={store.onWork} onPress={this._onChangeSwitchValue} />
+                            </View>
+                        </View>
+                    </View>
+                }
+                keyExtractor={this._keyExtractor}
+                data={store.orders.slice().sort((a, b) => a.start_time < b.start_time)} // возможно, эта сортировка когда-то будет работать неправильно
+                renderItem={this._renderItem}
+                refreshing={this.state.refreshing}
+                onRefresh={this._onRefresh}
+                ListEmptyComponent={<Text style={styles.mainFontUserType}>Нет доступных заявок</Text>}
+            />
+        );
+    }
 
-	_topUpBalance = () => {
-		this.props.navigation.navigate('Balance');
-	};
+    _topUpBalance = () => {
+        this.props.navigation.navigate('Balance');
+    };
 
-	_keyExtractor = (item, index) => ' ' + item._id; // для идентификации каждой строки нужен key типа String
+    _keyExtractor = (item, index) => ' ' + item._id; // для идентификации каждой строки нужен key типа String
 
-	_onChangeSwitchValue = async () => {
-		if (this.props.store.isDriver && this.props.store.veh_width === '') {
-			this.setState({ message: 'Заполните данные об автомобиле' });
-		} else {
-			if (this.props.store.balance < 0) {
-				this.setState({ message: 'Вы не можете выполнять заказы при отрицательном балансе. Пополните баланс' });
-			} else {
-				if (!this.props.store.onWork) {
-					if (!Platform.OS === 'android') {
-						console.log(
-							'Oops, this will not work on Sketch in an Android emulator. Try it on your device!'
-						);
-					} else {
-						this.props.store.setOnWork(!this.props.store.onWork);
-						
-                        await NativeModules.ForegroundTaskModule.startService(await AsyncStorage.getItem('token'));
-                        this._onRefresh();
-					}
-				} else {
-                    //TaskManager.unregisterAllTasksAsync();
-                    this.props.store.setOnWork(!this.props.store.onWork);
-                    await NativeModules.ForegroundTaskModule.stopService();
-                    this._onRefresh();
-				}
-			}
-		}
-	};
+    _onChangeSwitchValue = async () => {
+        const { status } = await Permissions.askAsync(Permissions.LOCATION);
 
-	_onPressOrderItemButton = order => {
-		this.props.navigation.navigate('OrderPreview', { order });
-	};
+        if (status !== 'granted') {
+            Alert.alert('Внимание', 'Для работы с приложением вам необходимо предоставить доступ к геолокации');
+            return;
+        }
 
-	_onRefresh = async () => {
-		//  const {updateUserInfo, getOrders} = this.props.store; так делать нельзя! mobx не сможет отследить вызов функции
-		this.setState({ message: '' });
-		if (this.props.store.onWork) {
-			const { store } = this.props;
-			//  this.fetchData();
-			this.setState({ refreshing: true });
+        if (this.props.store.isDriver && this.props.store.veh_width === '') {
+            this.setState({ message: 'Заполните данные об автомобиле' });
+            return;
+        }
 
-			const UserInfoPromise = store.updateUserInfo();
-			const OrdersPromise = store.getOrders();
+        if (this.props.store.balance < 0) {
+            this.setState({ message: 'Вы не можете выполнять заказы при отрицательном балансе. Пополните баланс' });
+            return;
+        }
 
-			await Promise.all([UserInfoPromise, OrdersPromise]);
+        if (!this.props.store.onWork) {
+            if (!Platform.OS === 'android') {
+                console.log('Oops, this will not work on Sketch in an Android emulator. Try it on your device!');
+            } else {
+                this.props.store.setOnWork(!this.props.store.onWork);
 
-			this.setState({ refreshing: false });
-		} else {
-			await this.props.store.updateUserInfo();
-			this.props.store.clearOrders();
-		}
-	};
+                await NativeModules.ForegroundTaskModule.startService(await AsyncStorage.getItem('token'));
+                this._onRefresh();
+            }
+        } else {
+            this.props.store.setOnWork(!this.props.store.onWork);
+            await NativeModules.ForegroundTaskModule.stopService();
+            this._onRefresh();
+        }
+    };
 
-	_renderItem = ({ item }) => (
-		<OrderCard
-			order={item}
-			time={item.start_time}
-			addresses={item.locations}
-			description={item.comment}
-			cardStyle={styles.cardMargins}
-			onPressButton={this._onPressOrderItemButton}
-			buttonName='ПРИНЯТЬ'
-		/>
-	);
+    _onPressOrderItemButton = order => {
+        this.props.navigation.navigate('OrderPreview', { order });
+    };
+
+    _onRefresh = async () => {
+        //  const {updateUserInfo, getOrders} = this.props.store; так делать нельзя! mobx не сможет отследить вызов функции
+        this.setState({ message: '' });
+        if (this.props.store.onWork) {
+            const { store } = this.props;
+            //  this.fetchData();
+            this.setState({ refreshing: true });
+
+            const UserInfoPromise = store.updateUserInfo();
+            const OrdersPromise = store.getOrders();
+
+            await Promise.all([UserInfoPromise, OrdersPromise]);
+
+            this.setState({ refreshing: false });
+        } else {
+            await this.props.store.updateUserInfo();
+            this.props.store.clearOrders();
+        }
+    };
+
+    _renderItem = ({ item }) => (
+        <OrderCard
+            order={item}
+            time={item.start_time}
+            addresses={item.locations}
+            description={item.comment}
+            cardStyle={styles.cardMargins}
+            onPressButton={this._onPressOrderItemButton}
+            buttonName='ПРИНЯТЬ'
+        />
+    );
 }
 
 export default MainScreen;
