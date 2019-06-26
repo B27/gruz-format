e010ec7e41709b58d03fa0970562206a02a06a82
B27 <ilia.burdukovski@ya.ru>
2019-06-26 19:09:52 +0800

    add android folder to metro bundler blacklist update version code to 1.0.5

--- App.js
+++ App.js
@@ -8,10 +8,17 @@ import NotificationListener from './utils/NotificationListener';
 import UniversalEventEmitter from './utils/UniversalEventEmitter';
 
 axios.defaults.baseURL = 'https://gruz.bw2api.ru'; /* 'http://192.168.1.4:3008'; */
+const TAG = '~App.js~';
 
 export default class App extends React.Component {
     componentDidMount() {
-        this.onMessageReceivedListener = UniversalEventEmitter.addListener('onMessageReceived', NotificationListener);
+        if (!this.onMessageReceivedListener) {
+            console.log(TAG, 'add new NotificationListener');
+            this.onMessageReceivedListener = UniversalEventEmitter.addListener(
+                'onMessageReceived',
+                NotificationListener
+            );
+        }
     }
 
     componentWillUnmount() {
@@ -19,8 +26,10 @@ export default class App extends React.Component {
         //     const socket = await getSocket();
         //     socket.emit('setWork', false);
         // })();
+        console.log(TAG, 'component unmount');
 
         if (this.onMessageReceivedListener) {
+            console.log(TAG, 'remove NotificationListener');
             this.onMessageReceivedListener.remove();
         }
     }
