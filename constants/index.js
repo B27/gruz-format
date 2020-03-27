b2e4d9a244bbf207d3bda1c30933ad47406ed3cf
B27 <ilia.burdukovski@ya.ru>
2020-03-27 17:56:25 +0800

    switch to release url

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-//export const URL = 'https://api.gruzformat.ru';
-export const URL = 'http://localhost:3008';
+export const URL = 'https://api.gruzformat.ru';
+// export const URL = 'http://localhost:3008';
 export const privacyPolicyURL = `${URL}/policy.pdf`;
 console.log('[Index].() URL', URL);
