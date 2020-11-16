037a4544a21f478bedfb5565360949af5be8dacf
B27 <ilia.burdukovski@ya.ru>
2020-11-16 23:53:04 +0800

    refactor(ios): bump version to 1.0.2 (10)

--- components/registerForPushNotificationsAsync.js
+++ components/registerForPushNotificationsAsync.js
@@ -34,7 +34,7 @@ export default async function registerForPushNotificationsAsync(userHasPushToken
             break;
 
         case 'ios':
-            body.appVersion = { android: 0, ios: 9 };
+            body.appVersion = { android: 0, ios: 10 };
             break;
     }
 
